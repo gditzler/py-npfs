609e48cb2d893dd69cedd89ca720a53c6e266daf
Gregory Ditzler <gregory.ditzler@gmail.com>
2014-04-05 00:17:16 -0400

    added __about__ variables

--- src/setup.py
+++ src/setup.py
@@ -1,3 +1,12 @@
 from distutils.core import setup
 
+__author__ = "Gregory Ditzler"
+__copyright__ = "Copyright 2014, EESI Laboratory (Drexel University)"
+__credits__ = ["Gregory Ditzler"]
+__license__ = "GPL"
+__version__ = "0.1.0"
+__maintainer__ = "Gregory Ditzler"
+__email__ = "gregory.ditzler@gmail.com"
+__status__ = "development"
+
 setup(name='npfs', version='0.1.0',py_modules=['npfs'])
